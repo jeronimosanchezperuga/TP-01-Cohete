38439c1d6d1d4a45c30a7db51235dac2fc3da88f
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2024-04-11 09:28:21 -0300

    Asteroid generation and randomization

---
+++ Assets/AsteroidScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidScript : MonoBehaviour
+{
+    public float rotationSpeed;
+    public float fallingSpeed;
+    public float maxScale;
+    public float minScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotationSpeed = Random.Range(-rotationSpeed, rotationSpeed);
+        transform.localScale *= Random.Range(minScale,maxScale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * fallingSpeed * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public GameObject asteroidPrefab;
+    public Vector3 newPosition;
+    public float minX;
+    public float maxX;
+    public float spawnInterval;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(CloneAsteroid), 0, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CloneAsteroid()
+    {
+        float randomx = Random.Range(minX, maxX);
+        newPosition = new Vector3(randomx, newPosition.y);
+        Instantiate(asteroidPrefab, newPosition, Quaternion.identity);
+    }
+}
